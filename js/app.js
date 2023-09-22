ef8ab31e55d2a88eb5c07020018ed0cbf1882377
Andrew Fennell <afennell@hotmail.co.uk>
2023-09-22 14:37:20 +0100

    problem five solved

--- js/app.js
+++ js/app.js
@@ -126,12 +126,24 @@ IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyArray() function and see if the test passes.*/
 
 // Write your code here
-function multiplyArray(multArr) {
-  //eslint-disable-line
+function multiplyArray(testArray) {
+  let productAb = multiply(testArray[0], testArray[1])[0];
+  let productAbc = multiply(productAb, testArray[2])[0];
+
+  var msg =
+    "The numbers " +
+    testArray.toString() +
+    " have a product of " +
+    productAbc +
+    ".";
+
+  return [productAbc, msg];
 }
+sumArray(testArray);
+//eslint-disable-line
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop.
 
